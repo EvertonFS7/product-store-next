c8ec16103e754cf630c69a98f806c685ec869fd2
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-06-27 20:35:40 -0300

    setup project

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+    config,
+    styled,
+    globalCss,
+    createTheme,
+    css,
+    keyframes,
+    getCssText,
+    theme
+} = createStitches({
+    theme: {
+        colors: {
+            default: '#8257e6'
+        }
+    }
+})
